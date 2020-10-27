b6427aaabd3b4e27f40659918afcc507962cefc3
Stanislav <stani.petrosyan@gmail.com>
2020-10-27 14:12:36 +0100

    refactor jstrace-bars

--- test/test-display.js
+++ test/test-display.js
@@ -3,9 +3,9 @@ const timeout = ms => new Promise(res => setTimeout(() => res(ms), ms));
 
 // test the usual
 console.log(barChart([
-  {label: 'smiles per day', count: 143},
-  {label: 'laughs', count: 340},
-  {label: 'high fives', count: 26}
+  { label: 'smiles per day', count: 143 },
+  { label: 'laughs', count: 340 },
+  { label: 'high fives', count: 26 }
 ]));
 
 // test progress bar
@@ -16,16 +16,16 @@ console.log(barChart([
     [1000], // arguments array provided to promise (timeout)
     n, // number of times to call promise
     ( // callback - invoked after each promise resolution
-    argsHandle, // modify this in the callback to change the arguments at the next invocation
-    previousResponse, // what is resolved from promise (timeout)
-    i) => {
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    const count = (i + 1) / n * 100;
-    const outputStr = barChart([{label: `${i + 1}/${n}`, count}], {percentages: true});
-    process.stdout.write(outputStr); // print the bar
-    argsHandle[0] = Math.max(previousResponse - 40, 10); // speed up over time
-  });
+      argsHandle, // modify this in the callback to change the arguments at the next invocation
+      previousResponse, // what is resolved from promise (timeout)
+      i) => {
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+      const count = (i + 1) / n * 100;
+      const outputStr = barChart([{ label: `${i + 1}/${n}`, count }], { percentages: true });
+      process.stdout.write(outputStr); // print the bar
+      argsHandle[0] = Math.max(previousResponse - 40, 10); // speed up over time
+    });
 })();
 
 function sequentialPromiseAll(func, args, num, updateCb) {
